03cf42478a9aac6f5bf19fc95d4a04e399f016d8
tmoon <tmoon@illinois.edu>
2024-01-30 10:39:59 -0600

    lec04/05

--- lec04/push_keyboard.asm
+++ lec04/push_keyboard.asm
@@ -1,15 +1,39 @@
 .ORIG x3000
 
 
-LOOP
+;1. first keyboard input
 GETC
 OUT
 JSR PUSH
-ADD   R5, R5, #0
-BRp   PUSH_FAIL
-BRnzp LOOP
+;omit overflow check
 
-PUSH_FAIL
+;2. second keyboard input
+GETC
+OUT
+JSR PUSH
+;omit overflow check
+
+;3. third keyboard input
+GETC
+OUT
+JSR PUSH
+;omit overflow check
+
+
+JSR POP
+;omit underflow check
+OUT
+
+JSR POP
+;omit underflow check
+OUT
+
+JSR POP
+;omit underflow check
+OUT
+
+
+
 HALT
 
 ;PUSH subroutine
@@ -50,9 +74,46 @@ LD	R6, SAVE_R6
 RET
 
 
+;POP subroutine
+;IN: none
+;OUT: R0 (value)
+;OUT: R5 (0: success, 1: fail)
+;R3: STACK_START
+;R6: STACK_TOP
+POP
+;callee-save & initialize registers
+	ST	R3,SAVE_R3
+	ST	R6,SAVE_R6	
+	LD	R3,STACK_START
+	LD	R6,STACK_TOP
+	AND	R5,R5,#0
+;underflow?
+;Check if STACK_TOP = STACK_START
+;Or check if STACK_TOP - STACK_START = 0
+	NOT	R3,R3
+	ADD	R3,R3,#1
+	ADD	R3,R3,R6
+	BRz	UNDERFLOW
+
+;it is NOT underflow
+	ADD	R6,R6,#1
+	LDR	R0,R6,#0
+	ST	R6,STACK_TOP
+	BRnzp	DONE_POP
+;it is underflow
+UNDERFLOW
+	ADD	R5,R5,#1
+
+;restore registers
+DONE_POP
+	LD	R3,SAVE_R3
+	LD	R6,SAVE_R6
+	RET
+
+
 STACK_END       .FILL   x3FFD
-STACK_START     .FILL   x3FFD
-STACK_TOP       .FILL   x3FFD
+STACK_START     .FILL   x4000
+STACK_TOP       .FILL   x4000
 SAVE_R3         .FILL    x0
 SAVE_R6         .FILL    x0
 
